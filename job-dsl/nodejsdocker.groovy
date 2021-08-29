65c7a385fce6436ad9d2d3f6971ece6b951645e8
Amit Wadhiani <amiwadhi@publicisgroupe.net>
2021-08-30 00:26:13 +0530

    added frontendJob

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -24,3 +24,30 @@ job('NodeJS Docker example') {
         }
     }
 }
+job('Angular RealWorld Frontend') {
+    scm {
+        git('https://github.com/amitwadhiani/frontendDemo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('amit.wadhiani@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install -g @angular/cli@latest && yarn install && ng build")
+        dockerBuildAndPublish {
+            repositoryName('amit0wadhiani/angularRealworldFrontend')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('998ffb10-3286-4316-9fad-ef16a26aaa52')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
